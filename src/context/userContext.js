c5e2b8a8b726e305ac8277d25ab5cf9d2c0176fc
shivagnanadeep <malepatignanadeep@gmail.com>
2025-01-19 17:40:16 +0530

    deleteing context folder

--- src/context/userContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const userContext = React.createContext({
-	users: [],
-	response: 0,
-	getProduct: () => {},
-	getAllProducts: () => {},
-});
-export default userContext;
